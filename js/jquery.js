4eb2f5389b24c7a625642cb4eba0f4d0756ffb56
“Daniel <danielcdeng@gmail.com>
2015-04-08 21:35:09 -0700

    Make jQuery code safe; some updates on about.html, alpha.htmml, home.html and index.html

--- js/jquery.js
+++ js/jquery.js
@@ -1,18 +1,21 @@
-$(document).ready(function() {
-    $('#menu-wrap').prepend('<div id="menu-trigger">Menu</div>');
-    $('#menu-trigger').on('click', function() {
-        $('#menu').slideToggle();
+// Safe jQuery code
+(function(global, $) {
+    $(document).ready(function() {
+        $('#menu-wrap').prepend('<div id="menu-trigger">Menu</div>');
+        $('#menu-trigger').on('click', function() {
+            $('#menu').slideToggle();
+        });
+        $(global).resize(function() {
+            var size = {
+                width: global.innerWidth || document.body.clientWidth,
+                //height: window.innerHeight || document.body.clientHeight
+            }
+            if(size.width > 700) {
+                $('#menu').show();
+            }
+            else {
+                $('#menu').hide();
+            }
+        });
     });
-    $(window).resize(function() {
-        var size = {
-            width: window.innerWidth || document.body.clientWidth,
-            //height: window.innerHeight || document.body.clientHeight
-        }
-        if(size.width > 700) {
-            $('#menu').show();
-        }
-        else {
-            $('#menu').hide();
-        }
-    });
-});
+}(window, jQuery));
